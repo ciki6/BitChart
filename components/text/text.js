fdb164784f8518edddb161b6c76e43c028e71cdb
ciki6 <18694942447@163.com>
2021-01-25 16:04:40 +0800

    update text.js

--- components/text/text.js
+++ components/text/text.js
@@ -1,3 +1,11 @@
-class Text extends ComponentBase{
-    constructor()
+class Text extends DIVComponentBase{
+    constructor(id, code, container, workMode, option = {}, useDefaultOpt = true) {
+        super(id, code, container, workMode, option, useDefaultOpt);
+        this._setupDefaultValues();
+        this._draw();
+        this.workMode = workMode;
+        if (workMode !== 2 && workMode !== 0) {
+            this._getWSData();
+        }
+    }
 }
